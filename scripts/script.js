52e55487a8f82edd900db294f1a98549ba0785bc
Pavel Spiridonov <foxtrot8900@gmail.com>
2022-02-02 14:12:14 +0300

    fixed_third

--- scripts/script.js
+++ scripts/script.js
@@ -4,18 +4,20 @@ let closebutton = document.querySelector('.popup__container-closeicon');
 let formelement = document.querySelector('form');
 let nameinput = document.querySelector('.popup__input_field_name');
 let jobinput = document.querySelector('.popup__input_field_job');
+let name = document.querySelector('.profile__info-title');
+let job = document.querySelector('.profile__info-subtitle');
 function openpopup() {
-    pop.classList.add('popup__container_active');
+    pop.classList.add('popup_active');
+    name.textContent=nameinput.value;
+    job.textContent=jobinput.value;
 }
 function closepopup() {
-    pop.classList.remove('popup__container_active');
+    pop.classList.remove('popup_active');
 }
 function formsubmitHandler (evt){
     evt.preventDefault();
     let namevalue = nameinput.value;
     let jobvalue = jobinput.value;
-    let name = document.querySelector('.profile__info-title');
-    let job = document.querySelector('.profile__info-subtitle');
     name.textContent=namevalue;
     job.textContent=jobvalue;
     closepopup();
